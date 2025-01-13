dbf469ade3f6925eb5eb6ba73a91b3de14e890da
Mr-Ganesh2005 <ganeshkumar.ai23@bmsce.ac.in>
2025-01-13 18:05:35 +0530

    Update Game_of_two_stacks.py

--- Game_of_two_stacks.py
+++ Game_of_two_stacks.py
@@ -1,20 +1,10 @@
-#!/bin/python3
+
 
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'twoStacks' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER maxSum
-#  2. INTEGER_ARRAY a
-#  3. INTEGER_ARRAY b
-#
 
 def twoStacks(maxSum, a, b):
     sum = 0
